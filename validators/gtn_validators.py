7584a2033de89dab3599a7cc04038b975faa1a02
Georgi St. Lukanov <goshiii87@gmail.com>
2025-07-18 17:40:06 +0300

    add validators folder and create gtn_validator.py

---
+++ validators/gtn_validators.py
@@ -0,0 +1,16 @@
+# validators/gtn_validators.py
+
+import pandas as pd
+
+
+def check_gtn_numeric_elements(gtn_df: pd.DataFrame, mapping: dict):
+    """
+    Check that all mapped GTN element columns contain numeric values.
+    Returns a list of element names that contain non-numeric values.
+    """
+    non_numeric_columns = []
+    for vendor_col in mapping['used'].keys():
+        if vendor_col in gtn_df.columns:
+            if not pd.to_numeric(gtn_df[vendor_col], errors='coerce').notna().all():
+                non_numeric_columns.append(vendor_col)
+    return non_numeric_columns
